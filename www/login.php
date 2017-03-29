2a679e4651beb6cb4d23153d0b5965531fe05074
ololade <adedoyin.ololade83@gmail.com>
2017-03-29 14:50:54 +0100

    login done and dusted

--- www/login.php
+++ www/login.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Register</title>
-	<link rel="stylesheet" type="text/css" href="../styles/styles.css">
-</head>
-<body>
-	<section>
-		<div class="mast">
-			<h1>T<span>SSB</span></h1>
-		</div>
-	</section>
+<?php
+#include header
+	include 'includes/header.php';
+
+
+?>
+
 
 
 <div class="wrapper">
@@ -31,11 +26,9 @@
 		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
 	</div>
 
-	<section class="foot">
-		<div>
-			<p>&copy; 2016;
-		</div>
-	</section>
-</body>
-</html>
+	<?php
+#include header
+	include 'includes/footer.php';
 
+
+?>
